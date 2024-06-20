547baea07539868ddaec89d380b0111ea1579e3e
Anton Knyazev <147397298+DEV-m1k0@users.noreply.github.com>
2024-06-20 11:32:54 +0300

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,6 +79,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+print(os.getenv("MYSQL_DBNAME"))
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
